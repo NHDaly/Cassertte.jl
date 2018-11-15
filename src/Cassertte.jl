830ad5d3a2ecdef4bc393be7bbb21db219ab9d1f
Nathan Daly <nhdaly@gmail.com>
2018-11-14 21:39:08 -0500

    Added comments

--- src/Cassertte.jl
+++ src/Cassertte.jl
@@ -4,6 +4,20 @@ export @Cassertte, @withCassertte
 
 using Toggles
 
+"""
+    @Cassertte length(unique(elements)) == 10
+An @assert replacement that only evaluates its argument expr when called via @withCassertte.
+
+@Cassertte is disabled by default, and can be enabled in calling code via @withCassertte.
+Importantly, the _entire expression_ is eliminated. This frees you to check expensive
+calculations in your assert statement. But of course, it means that you should never
+depend on the side effects of an expression within a @Cassertte.
+
+To run your code with all @Cassertte statements enabled, use @withCassertte:
+```
+    @withCassertte foo(1,2)
+```
+"""
 macro Cassertte(expr)
     quote
         Toggles.@toggleable(nothing, CassertteSwitch(), @assert($(esc(expr)), $(string(expr))))
@@ -12,6 +26,11 @@ end
 
 struct CassertteSwitch end
 
+"""
+    @withCassertte foo(x,y,z)
+Call a function with @Cassertte assertions enabled. Note that this causes the entire
+expression inside any @Cassertte statements to be evaluated.
+ """
 macro withCassertte(expr)
     quote Toggles.@toggle(CassertteSwitch(), $(esc(expr))) end
 end
